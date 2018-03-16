9090e5719da480d205a62fc59c16ef6bdb51b949
ayadij <ayadijkwel@yahoo.com>
2018-03-16 10:02:10 -0600

    bowling score keeper

---
+++ bowling/with-classes/bowling.rb
@@ -0,0 +1,53 @@
+
+
+#if throw1 + throw2 >= 10 , spare = throw1 + throw2
+# spare_score = 10 + throw3
+
+
+
+# roll(pins : int)
+# score() : int #called at the very end of the game 
+
+require_relative "./frame"
+
+class Game
+
+  FRAME_COUNT = 10
+
+  def initialize
+    tail_frame = Frame.new
+
+    FRAME_COUNT.times { tail_frame = Frame.new(tail_frame) }
+
+    @head_frame = tail_frame
+    @current_frame = tail_frame
+  end
+
+  def roll(pins)
+    @current_frame.rolls << pins
+    if @current_frame.closed?
+      @current_frame = @current_frame.next_frame
+    end
+  end
+
+  def score
+    frames
+      .flat_map(&:score)
+      .inject(0, :+)
+  end
+
+  def frames
+    list = []
+    this_frame = @head_frame
+    while this_frame
+      list << this_frame
+      this_frame = this_frame.next_frame
+    end
+    list
+  end
+
+end
+
+
+
+
---
+++ bowling/with-classes/frame.rb
@@ -0,0 +1,76 @@
+class Frame
+
+  PIN_COUNT = 10
+  MAX_ROLLS = 2
+
+  attr_accessor :rolls
+  attr_reader :next_frame
+
+  def initialize(next_frame = nil)
+    @rolls = []
+    @next_frame = next_frame
+  end
+
+  def strike?
+    all_pins_down? && roll_count == 1
+  end
+
+  def spare?
+    all_pins_down? && maxed_rolls?
+  end
+
+  def open?
+    !closed?
+  end
+
+  def closed?
+    all_pins_down? || maxed_rolls?
+  end
+
+  def score
+    tally = pins_down_count
+    return tally if next_frame.nil?
+
+    rolls_to_consume = if strike?
+                         2
+                       elsif spare?
+                         1
+                       else
+                         0
+                       end
+
+    future_rolls = next_n_rolls(rolls_to_consume)
+    tally + future_rolls.inject(0, :+)
+  end
+
+  def pins_down_count
+    rolls.inject(0, :+)
+  end
+
+  private
+
+  def next_n_rolls(n_to_take)
+    to_take_from = next_frame
+    results = []
+    while to_take_from && results.length < n_to_take
+      taken = to_take_from.rolls.take(n_to_take)
+      results += taken
+      n_to_take -= taken.length
+      to_take_from = to_take_from.next_frame
+    end
+    results
+  end
+
+  def all_pins_down?
+    pins_down_count == PIN_COUNT
+  end
+
+  def maxed_rolls?
+    roll_count == MAX_ROLLS
+  end
+
+  def roll_count
+    rolls.length
+  end
+
+end
